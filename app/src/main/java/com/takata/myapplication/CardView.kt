137d9b62cce3e8d2376f64424e97faaa4e1ad445
Takata <felipetakatadealvarenga@gmail.com>
2022-11-03 11:15:30 -0300

    Feat Binding Cardview para Cadastro

--- app/src/main/java/com/takata/myapplication/CardView.kt
+++ app/src/main/java/com/takata/myapplication/CardView.kt
@@ -1,11 +1,26 @@
 package com.takata.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.takata.myapplication.databinding.ActivityCardViewBinding
+import com.takata.myapplication.databinding.ActivityMainBinding
+
 
 class CardView : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCardViewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_card_view)
+        binding = ActivityCardViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.adicionar.setOnClickListener(){
+            val intent = Intent(this, Cadastro :: class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
