e11157eebf6b9c9cfcf459542ad765c72621f6f0
gmilanimota <gustavo.milani22@gmail.com>
2022-11-03 21:21:12 -0300

    feat: Room database, Person Entity, DAO

---
+++ app/src/main/java/com/takata/myapplication/entities/Person.kt
@@ -0,0 +1,15 @@
+package com.takata.myapplication.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Person (
+    var name:String = "",
+    var email:String = "",
+    var birthDate:String = "",
+    var phone:String = "",
+    var address:String = ""
+) {
+    @PrimaryKey(autoGenerate = true) var id:Int = 0
+}
